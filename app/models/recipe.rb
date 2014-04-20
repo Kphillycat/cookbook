a32b41771508ec98307ccb087f922f5ead562f24
Kphillycat <keithwilliamsjr@gmail.com>
2014-04-20 16:40:24 -0400

    Add logic for parsing of fractions in ingredients textarea

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,8 +5,9 @@ class Recipe < ActiveRecord::Base
     recipe = Recipe.new(:name => recipe_params[:name], :description => recipe_params[:description], :prep_instructions => recipe_params[:prep_instructions])
 
     recipe_params[:ingredients][:ingredients].split(",").each do |ingredient|
-      ingredient_match = ingredient.match(/(.?\d+) (\w+) (\w+)/)
-      current_ingredient = Ingredient.new(:quantity => ingredient_match[1].to_f, :unit => ingredient_match[2], :name => ingredient_match[3])
+      ingredient_match = ingredient.match(/([\d+]?.?\/?\d+) (\w+) (\w+)/)
+      ingredient_qty = ingredient_match[1].convert_from_fraction if ingredient_match[1].index("/")
+      current_ingredient = Ingredient.new(:quantity => ingredient_qty || ingredient_match[1].to_f, :unit => ingredient_match[2], :name => ingredient_match[3])
       recipe.ingredients << current_ingredient     
     end
     recipe
@@ -16,10 +17,21 @@ class Recipe < ActiveRecord::Base
     self.update(:name => recipe_params[:name], :description => recipe_params[:description], :prep_instructions => recipe_params[:prep_instructions])
     
     recipe_params[:ingredients][:ingredients].split(",").each do |ingredient|
-      ingredient_match = ingredient.match(/(.?\d+) (\w+) (\w+)/)
-      current_ingredient = Ingredient.new(:quantity => ingredient_match[1].to_f, :unit => ingredient_match[2], :name => ingredient_match[3])
+      ingredient_match = ingredient.match(/([\d+]?.?\/?\d+) (\w+) (\w+)/)
+      ingredient_qty = ingredient_match[1].convert_from_fraction if ingredient_match[1].index("/")
+
+      current_ingredient = Ingredient.new(:quantity => ingredient_qty || ingredient_match[1].to_f, :unit => ingredient_match[2], :name => ingredient_match[3])
       self.ingredients << current_ingredient     
     end
     self.save
   end
+end
+
+class String
+  def convert_from_fraction
+    numerator, denominator = split('/').map(&:to_f)
+    denominator ||= 1
+    numerator/denominator
+  end
+
 end
