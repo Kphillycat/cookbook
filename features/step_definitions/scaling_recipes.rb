d0fc8c938d884c4e5e6632882875446e9a39a5d8
Kphillycat <keithwilliamsjr@gmail.com>
2014-04-20 11:49:24 -0400

    Remove pluralize portion of scaling feature test since angular can't take advantage of rails pluralize method. Will add front end pluralize logic later

--- features/step_definitions/scaling_recipes.rb
+++ features/step_definitions/scaling_recipes.rb
@@ -3,5 +3,5 @@ When(/^I enter in the number "(.*?)"$/) do |num|
 end
 
 Then(/^I expect to see the recipe scale up by a factor of "(.*?)"$/) do |num|
-  expect(page).to have_content("#{num.to_i * @recipe.ingredients[0].quantity} #{@recipe.ingredients[0].unit.pluralize(num)} of #{@recipe.ingredients[0].name}")  
+  expect(page).to have_content("#{num.to_i * @recipe.ingredients[0].quantity} #{@recipe.ingredients[0].unit} of #{@recipe.ingredients[0].name}")  
 end
--- features/step_definitions/viewing_recipe_details.rb
+++ features/step_definitions/viewing_recipe_details.rb
@@ -1,6 +1,5 @@
 When(/^I click on the details link$/) do
   page.find("#recipe-detail-link").click
-  save_and_open_page
 end
 
 Then(/^I expect to see the details of that recipe$/) do
