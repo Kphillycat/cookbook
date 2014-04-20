6be2cdf0dc025139f90b1c4a1fd0c5046dfa62b4
Kphillycat <keithwilliamsjr@gmail.com>
2014-04-20 13:51:24 -0400

    Remove app folder of angular assets in public directory

--- public/app/js/filters.js
+++
@@ -1,5 +0,0 @@
-angular.module('spacecatFilters', []).filter('checkmark', function() {
-  return function(input){
-    return input ? '\u2713' : '\u2718';
-  }
-})
--- public/app/js/services.js
+++
@@ -1,8 +0,0 @@
-var recipeServices = angular.module('recipeServices', ['ngResource']);
-
-recipeServices.factory('Recipe', ['$resource',
-  function($resource){
-    return $resource('http://localhost:3000/api/recipes.json', {}, {
-      query: {method: 'GET', params: {}, isArray:true}
-    });
-  }]);
