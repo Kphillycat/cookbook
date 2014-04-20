bde29516e0d0b5768d8fe55d1590eba28137a693
Kphillycat <keithwilliamsjr@gmail.com>
2014-04-20 12:39:02 -0400

    Add rails to precompile static assets

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(:default, Rails.env)
 module Cookbook
   class Application < Rails::Application
 
+    config.serve_static_assets = true
     config.assets.precompile << 'javascript/angular'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
