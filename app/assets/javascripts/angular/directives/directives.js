1f636c706e4e71657a78e10741f465ddfe4ed465
Kphillycat <keithwilliamsjr@gmail.com>
2014-04-20 09:44:02 -0400

    Slimed down index page by removing the prep details and description and only having a link to the details page

--- app/assets/javascripts/angular/directives/directives.js
+++ app/assets/javascripts/angular/directives/directives.js
@@ -1,12 +1,14 @@
 'use strict';
 
 recipeApp.directive('recipeSnippet',function(){
-    return function(scope, element, attr) {
-      scope.$watch('currentRecipe', function(){
+    return function(scope, element, attrs) {
+      scope.$watch('currentRecipe', function(attrs){
         // debugger;
         if (scope.showDetail){
-          debugger;
-          element.find("recipe-detail").removeClass("hidden");
+          // debugger;
+          element.find("recipe-detail").slideDown(2);
+ //          element.find("recipe-detail").removeClass("hidden").wrapInner('<div style="display: none;" />')
+ // .parent().find('td > div').slideDown(2);
         }
         // element.slideDown(2);
       })         
@@ -17,12 +19,5 @@ recipeApp.directive('recipeDetail', function() {
     return {
         restrict: 'E',
         templateUrl: "angular/templates/recipe_detail.html"
-//         // controller: function($scope) {
-//         //     $scope.spinning = true;
-//         //     $scope.stopOrStart = function() {
-//         //         $scope.spinning = !$scope.spinning;
-//         //     };
-//         // },
-//         // templateUrl: 'gear-widget.html'
     };
 });
