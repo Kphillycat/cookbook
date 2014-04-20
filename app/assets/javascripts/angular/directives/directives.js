b01b852f9be2f1556aa3a60e8f8b55061b37df3d
Kphillycat <keithwilliamsjr@gmail.com>
2014-04-20 09:58:40 -0400

    Change recipe details display logic from removeclass to toggle

--- app/assets/javascripts/angular/directives/directives.js
+++ app/assets/javascripts/angular/directives/directives.js
@@ -4,12 +4,14 @@ recipeApp.directive('recipeSnippet',function(){
     return function(scope, element, attrs) {
       scope.$watch('currentRecipe', function(attrs){
         // debugger;
-        if (scope.showDetail){
+
+        element.find("recipe-detail").toggle();
+        // if (scope.showDetail){
           // debugger;
-          element.find("recipe-detail").slideDown(2);
+          // element.find("recipe-detail").slideDown(2);
  //          element.find("recipe-detail").removeClass("hidden").wrapInner('<div style="display: none;" />')
  // .parent().find('td > div').slideDown(2);
-        }
+        // }
         // element.slideDown(2);
       })         
     }
