fc23d2ace38b73e631e0be974cb2a45de51c52c3
Kphillycat <keithwilliamsjr@gmail.com>
2014-04-20 13:09:53 -0400

    Add query factory

--- app/assets/javascripts/angular/controllers/controllers.js
+++ app/assets/javascripts/angular/controllers/controllers.js
@@ -4,9 +4,7 @@ var recipeControllers = angular.module('recipeControllers', []);
 
 recipeApp.controller('RecipeListCtrl', ['$scope', '$http', 'Recipe',
   function($scope, $http, Recipe){
-    $http.get('api/recipes.json').success(function(data) {
-      $scope.recipes = data;
-    });
+    $scope.recipes = Recipe.query();
     
     $scope.showRecipe = function(recipeId){
       $http.get("api/recipes/" + recipeId + '.json').success(function(data) {
--- app/assets/javascripts/angular/services/services.js
+++ app/assets/javascripts/angular/services/services.js
@@ -2,7 +2,7 @@ var recipeServices = angular.module('recipeServices', ['ngResource']);
 
 recipeServices.factory('Recipe', ['$resource',
   function($resource){
-    return $resource('http://localhost:3000/api/recipes.json', {}, {
+    return $resource('/api/recipes.json', {}, {
       query: {method: 'GET', params: {}, isArray:true}
     });
   }]);
